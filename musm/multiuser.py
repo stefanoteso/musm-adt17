e2a71d1a45a105d5f63ad267b7779498d750c344
Stefano Teso <stefano.teso@gmail.com>
2017-03-26 09:50:36 +0200

    multiuser: update to best so far.

--- musm/multiuser.py
+++ musm/multiuser.py
@@ -3,6 +3,7 @@ import itertools as it
 from scipy.spatial.distance import pdist
 from sklearn.cross_validation import KFold
 from sklearn.utils import check_random_state
+from textwrap import dedent
 from time import time
 
 from . import get_logger
@@ -21,17 +22,23 @@ _DEFAULT_ALPHA = 10, 0.1, 0.1
 _NUM_FOLDS = 5
 
 
-def logistic(x):
-    return 1 / (1 + np.exp(-x))
+def normalize(w):
+    if w.ndim == 1:
+        return w / np.linalg.norm(w)
+    else:
+        v = np.array(w, copy=True)
+        for i in range(len(v)):
+            v[i] /= (np.linalg.norm(v[i]) + 1e-13)
+        return v
 
 
-def crossvalidate(problem, dataset, set_size, uid, weights, var, cov,
-                  transform, old_alpha):
+def crossvalidate(problem, dataset, set_size, uid, w, var, cov,
+                  transform, lmbda, old_alpha):
     if len(dataset) % _NUM_FOLDS != 0:
         return old_alpha
 
     kfold = KFold(len(dataset), n_folds=_NUM_FOLDS)
-    p = compute_transform(weights, cov, var, transform, uid)
+    p = compute_transform(uid, w, cov, var, transform, lmbda)
 
     avg_accuracy = np.zeros(len(_ALPHAS))
     for i, alpha in enumerate(_ALPHAS):
@@ -46,8 +53,7 @@ def crossvalidate(problem, dataset, set_size, uid, weights, var, cov,
     alpha = _I_TO_ALPHA[np.argmax(avg_accuracy)]
 
     _LOG.debug('''\
-            crossvalidation says:
-            avg_accuracy = {avg_accuracy}
+            alpha accuracies = {avg_accuracy}
             best alpha = {alpha}
         ''', **locals())
 
@@ -64,55 +70,88 @@ def select_user(var, satisfied_users, rng):
     return uid
 
 
-def compute_var_cov(w):
-    if w.ndim == 2:
-        num_users, set_size = len(w), 1
-    else:
-        num_users, set_size = w.shape[0], w.shape[1]
+def compute_var_cov(uid_to_w):
+    num_users = len(uid_to_w)
 
-    var = np.zeros(num_users)
-    for uid in range(num_users):
+    known_users = [uid for uid, w in uid_to_w.items() if w is not None]
+    set_size = uid_to_w[known_users[0]].shape[0] if len(known_users) else 1
+
+    var = np.ones(num_users)
+    for uid in known_users:
         for i, j in it.product(range(set_size), repeat=2):
-            diff = (w[uid,i] / np.linalg.norm(w[uid,i]) -
-                    w[uid,j] / np.linalg.norm(w[uid,j]))
+            if j <= i:
+                continue
+            diff = uid_to_w[uid][i] - uid_to_w[uid][j]
             var[uid] += np.dot(diff, diff)
-    var /= 4
+    if set_size >= 2:
+        # XXX this is a loose upper bound to the max variance
+        var[known_users] /= 2 * set_size * (set_size - 1)
 
-    cov = np.zeros((num_users, num_users))
-    for uid1, uid2 in it.product(range(num_users), repeat=2):
-        p = np.dot(w[uid1], w[uid2].T).sum()
-        q1 = np.dot(w[uid1], w[uid1].T).sum()
-        q2 = np.dot(w[uid2], w[uid2].T).sum()
-        cov[uid1, uid2] = p / np.sqrt(q1 * q2)
+    assert (var >= 0).all(), 'var is negative'
+    assert (var <= 1).all(), 'var is too large'
+
+    cov = np.eye(num_users)
+    for uid, vid in it.product(known_users, known_users):
+        p = np.dot(uid_to_w[uid], uid_to_w[vid].T).min()
+        q = np.dot(uid_to_w[uid], uid_to_w[uid].T).min() * \
+            np.dot(uid_to_w[vid], uid_to_w[vid].T).min()
+        if q == 0:
+            cov[uid, vid] = 1
+        else:
+            cov[uid, vid] = p / np.sqrt(q)
+
+    # XXX for some reason the second assertion triggers for indep
+
+    assert (cov >= 0).all(), 'cov is negative'
+    assert (cov <= 1 + 1e-1).all(), 'cov is too large, cov =\n{}'.format(cov)
 
     return var, cov
 
 
-def compute_transform(weights, cov, var, transform, uid):
-    if transform == 'indep':
-        return 1, 1
+def compute_transform(uid, uid_to_w, var, cov, transform, lmbda):
+    others = [vid for vid in range(len(var))
+              if vid != uid and uid_to_w[vid] is not None]
 
-    avg_weights = weights.mean(axis=1)
-    others = [i for i in range(len(var)) if i != uid]
+    if transform == 'indep' or not len(others):
+        return 1, 0
+
+    uid_to_w1 = {vid: uid_to_w[vid].mean(axis=0) for vid in others}
+
     if transform == 'sumcov':
-        a = 1
-        b = logistic(np.dot(cov[uid, others], avg_weights[others]))
-    elif transform == 'varsumvarcov':
-        a = 1
-        b = var[uid] * np.dot((1 - var)[others] * cov[uid, others],
-                              avg_weights[others])
+        a = lmbda
+        b = (1 - lmbda) * sum([cov[uid,vid] * uid_to_w1[vid] for vid in others])
+    elif transform == 'varsumcov':
+        a = (1 - var[uid])
+        b = var[uid] * sum([cov[uid,vid] * uid_to_w1[vid] for vid in others])
     else:
-        raise NotImplementedError('transform = {}'.format(transform))
+        raise NotImplementedError('invalid transform, {}'.format(transform))
 
-    assert a >= 0 and (b >= 0).all(), 'invalid transform {}, {}'.format(a, b)
+    # XXX suppress very small negative values
+    b[np.logical_and(-1e-2 <= b, b <= 0)] = 0
+
+    if a < 0 or (b < 0).any():
+        eigval, eigvec = np.linalg.eigh(cov)
+        msg = dedent('''\
+                uid = {uid}, others = {others}
+                var = {var}
+                cov =
+                {cov}
+                eigval(cov) =
+                {eigval}
+                eigvec(cov) =
+                {eigvec}
+                a = {a}
+                b = {b}
+            ''').format(**locals())
+        raise RuntimeError(msg)
+
     return a, b
 
 
 def musm(problem, group, set_size=2, max_iters=100, enable_cv=False,
-         transform='sumk', rng=None):
+         transform='indep', sources='all', lmbda=0.5, rng=None):
     rng = check_random_state(rng)
-
-    num_users, num_attributes = len(group), problem.num_attributes
+    num_users = len(group)
 
     group_str = '\n'.join([str(user) for user in group])
     _LOG.info('''\
@@ -120,64 +159,69 @@ def musm(problem, group, set_size=2, max_iters=100, enable_cv=False,
             {group_str}
         ''', **locals())
 
-    default_w, _ = problem.select_query([], set_size, _DEFAULT_ALPHA)
-    weights = np.array([default_w] * num_users)
-    var, cov = compute_var_cov(weights)
+    uid_to_w = {uid: None for uid in range(num_users)}
+    uid_to_w1 = {uid: None for uid in range(num_users)}
+    source_w = {'all': uid_to_w, 'best': uid_to_w1}[sources]
 
-    datasets = [np.empty((0, num_attributes)) for _ in group]
+    var, cov = compute_var_cov(source_w)
+
+    satisfied_users = set()
+    datasets = [np.empty((0, problem.num_attributes)) for _ in group]
     alphas = [_DEFAULT_ALPHA for _ in group]
 
-    trace, satisfied_users = [], set()
+    trace = []
     for t in range(max_iters):
         t0 = time()
-
         uid = select_user(var, satisfied_users, rng)
 
-        p = compute_transform(weights, cov, var, transform, uid)
+        f = compute_transform(uid, source_w, var, cov, transform, lmbda)
         w, query_set = problem.select_query(datasets[uid], set_size,
-                                            alphas[uid], transform=p)
-        weights[uid] = w
-
-        regrets_k = np.zeros(len(group))
-        for vid, user in enumerate(group):
-            regrets_k[vid] = min([user.regret(x) for x in query_set])
-
+                                            alphas[uid], transform=f)
+        uid_to_w[uid] = normalize(w)
         t0 = time() - t0
 
         i_star = group[uid].query_choice(query_set)
 
         t1 = time()
-
         for i in range(set_size):
             if i != i_star:
-                delta = (query_set[i_star] - query_set[i])
+                delta = (query_set[i_star] - query_set[i]).reshape(1, -1)
                 if (delta == 0).all():
                     _LOG.warning('all-zero delta added!')
-                datasets[uid] = np.append(datasets[uid], delta.reshape(1, -1),
-                                          axis=0)
+                datasets[uid] = np.append(datasets[uid], delta, axis=0)
 
-        var, cov = compute_var_cov(weights)
+        var, cov = compute_var_cov(source_w)
 
-        regrets_1 = np.zeros(len(group))
+        # XXX regretsk is only computed for uid!!!
+
+        regrets1, regretsk = np.zeros(num_users), np.zeros(num_users)
         for vid, user in enumerate(group):
-            vid_p = compute_transform(weights, cov, var, transform, vid)
-            _, x = problem.select_query(datasets[vid], 1,
-                                        alphas[uid], transform=vid_p)
-            regrets_1[vid] = user.regret(x[0])
+            f = compute_transform(vid, source_w, var, cov, transform, lmbda)
+            w, x = problem.select_query(datasets[vid], 1,
+                                        alphas[vid], transform=f)
+            uid_to_w1[vid] = normalize(w)
+
+            regrets1[vid] = user.regret(x[0])
+            regretsk[vid] = min([user.regret(x) for x in query_set])
+
             if user.is_satisfied(x[0]):
                 satisfied_users.add(vid)
-
         t1 = time() - t1
 
+
+        cov_str = '\n'.join([str(cov[vid]) for vid in range(num_users)]) + '\n'
         _LOG.debug('''\
                 ITERATION {t:3d}
-                user = {uid} {satisfied_users} {var}
-                regrets@1 = {regrets_1}
-                regrets@k = {regrets_k}
+                user = {uid} {satisfied_users}
+                regrets@1 = {regrets1}
+                regrets@k = {regretsk}
                 var = {var}
-                cov = {cov}
-                w = {w}
-                transform = {p}
+                cov =
+                {cov_str}
+                w =
+                {uid_to_w}
+                w1 =
+                {uid_to_w1}
                 q = {query_set}
                 i_star = {i_star}
                 datasets =
@@ -187,10 +231,11 @@ def musm(problem, group, set_size=2, max_iters=100, enable_cv=False,
         t2 = time()
         if enable_cv:
             alphas[uid] = crossvalidate(problem, datasets[uid], set_size, uid,
-                                        weights, var, cov, transform, alphas[uid])
+                                        source_w, var, cov, transform, lmbda,
+                                        alphas[uid])
         t2 = time() - t2
 
-        trace.append(list(regrets_1) + list(regrets_k) + [uid, t0 + t1 + t2])
+        trace.append(list(regrets1) + list(regretsk) + [uid, t0 + t1 + t2])
 
         if len(satisfied_users) == len(group):
             break
