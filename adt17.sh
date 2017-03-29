79f11324a6a31e8e6bff8f9226e8923e48fab16e
Stefano Teso <stefano.teso@gmail.com>
2017-03-29 12:42:31 +0200

    adt17: update the runner script for the latest experimental setting.

--- adt17.sh
+++ adt17.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-#MC_LIST=('2 1')
+N=10
+T=200
+S=best
+
 MC_LIST=('10 1' '10 2' '10 5')
 
 for i in `seq 0 $((${#MC_LIST[*]} - 1))`; do
@@ -8,17 +11,10 @@ for i in `seq 0 $((${#MC_LIST[*]} - 1))`; do
     C=`echo ${MC_LIST[$i]} | cut -d' ' -f2`
     for d in 0.5; do
         for K in 2; do
-
-            echo "RUNNING INDEPENDENT $M $C $d $K"
-            ./adt17.py synthetic -N 10 -M $M -C $C -T 50 -K $K -d $d --min-regret 0.1 -F indep
-
-            for S in best; do
-                for L in 0.25 0.5 0.75; do
-                    echo "RUNNING SUMCOV $M $C $d $K $S $L"
-                    ./adt17.py synthetic -N 10 -M $M -C $C -T 50 -K $K -d $d --min-regret 0.1 -F sumcov -S $S -L $L
-                done
+            for F in indep sumcov varsumvarcov; do
+                echo "RUNNING RF $M $C $d $K"
+                ./adt17.py synthetic -N $N -M $M -C $C -T $T -K $K -d $d --min-regret 0.1 -F $F -v
             done
-
         done
     done
 done
