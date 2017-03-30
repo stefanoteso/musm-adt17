cd4b6766ffb40dd830194b3e9fe8bea7eb3606c7
Stefano Teso <stefano.teso@gmail.com>
2017-03-30 14:42:16 +0200

    adt17: update the experimental setting (again).

--- adt17.sh
+++ adt17.sh
@@ -9,8 +9,8 @@ MC_LIST=('10 1' '10 2' '10 5')
 for i in `seq 0 $((${#MC_LIST[*]} - 1))`; do
     M=`echo ${MC_LIST[$i]} | cut -d' ' -f1`
     C=`echo ${MC_LIST[$i]} | cut -d' ' -f2`
-    for d in 0.5; do
-        for K in 2; do
+    for d in 0.5 1.0; do
+        for K in 2 3; do
             for F in indep sumcov varsumvarcov; do
                 echo "RUNNING RF $M $C $d $K"
                 ./adt17.py synthetic -N $N -M $M -C $C -T $T -K $K -d $d --min-regret 0.1 -F $F -v
